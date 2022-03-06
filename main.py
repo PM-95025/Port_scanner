5149a399e716eba3c9f26fe5207482d7d966eb88
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-03-06 21:00:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import socket
+from typing import Iterable
+from colorama import init, Fore
+from tqdm import tqdm
+
+init()
+GREEN = Fore.GREEN
+RESET = Fore.RESET
+
+opened: set[int] = set()
+
+host = input('host: ')
+socket.setdefaulttimeout(__timeout=float(input('timeout: ')))
+
+
+def port_scan(port: int):
+	s: socket = socket.socket()
+	try:
+		s.connect((host, port))
+	except (ConnectionError, TimeoutError):
+		return False
+	else:
+		return True
+	finally:
+		s.close()
+
+
+def scan(ports_: Iterable[int]):
+	for port in ports_:
+		if port_scan(port):
+			opened.add(port)
+			print(f"{GREEN}{host:15}:{port:5} is open    {RESET}")
+		pass
+
+
+with open('ports.txt') as f:
+	ports = [int(i) for i in f.readlines()]
+	print(f'will bo checked {len(ports)} ports')
+
+scan(ports)
+print(', '.join([str(i) for i in list(opened)]))
