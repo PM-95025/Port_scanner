4360c16f8a3f3dad5c9b505127968564dc6648c6
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-03-07 00:36:59 +0300

    added 'no inet' state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import socket
-from typing import Iterable
+from typing import Iterable, Union
 from colorama import init, Fore
 from tqdm import tqdm
 
@@ -10,15 +10,18 @@ RESET = Fore.RESET
 opened: set[int] = set()
 
 host = input('host: ')
-socket.setdefaulttimeout(__timeout=float(input('timeout: ')))
+socket.setdefaulttimeout(float(input('timeout: ')))
 
 
-def port_scan(port: int) -> bool:
+def port_scan(port: int) -> Union[bool, str]:
 	s: socket = socket.socket()
 	try:
 		s.connect((host, port))
 	except (ConnectionError, TimeoutError):
 		return False
+	except OSError:
+		print('No internet connection!')
+		return 'no'
 	else:
 		return True
 	finally:
@@ -27,15 +30,19 @@ def port_scan(port: int) -> bool:
 
 def scan(ports_: Iterable[int]) -> None:
 	for port in list(ports_):
-		if port_scan(port):
+		state = port_scan(port)
+		if state == 'no':
+			return
+		elif not state:
+			pass
+		else:
 			opened.add(port)
 			print(f"{GREEN}{host:15}:{port:5} is open    {RESET}")
-		pass
 
 
 with open('ports.txt') as f:
 	ports: tuple[int] = tuple([int(i) for i in f.readlines()])
-	print(f'will bo checked {len(ports)} ports')
+	print(f'will be checked {len(ports)} ports')
 
 scan(ports)
 print(', '.join([str(i) for i in list(opened)]))
