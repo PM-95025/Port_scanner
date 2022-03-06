58ffabca1affe71dafcc52b1a688dca765e360f3
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-03-07 00:53:22 +0300

    changed offline behavior

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import socket
-from typing import Iterable, Union
+from typing import Final, Iterable
 from colorama import init, Fore
+import sys
 from tqdm import tqdm
 
 init()
-GREEN = Fore.GREEN
-RESET = Fore.RESET
+GREEN: Final[str] = Fore.GREEN
+RESET: Final[str] = Fore.RESET
 
 opened: set[int] = set()
 
-host = input('host: ')
+try:
+	socket.socket().connect(('8.8.8.8', 53))
+except socket.error:
+	print("This script requires established interned connection!")
+	sys.exit()
+
+
+host: Final[str] = input('host: ')
 socket.setdefaulttimeout(float(input('timeout: ')))
 
 
-def port_scan(port: int) -> Union[bool, str]:
+def port_scan(port: int) -> bool:
 	s: socket = socket.socket()
 	try:
 		s.connect((host, port))
 	except (ConnectionError, TimeoutError):
 		return False
-	except OSError:
-		print('No internet connection!')
-		return 'no'
 	else:
 		return True
 	finally:
@@ -30,18 +35,14 @@ def port_scan(port: int) -> Union[bool, str]:
 
 def scan(ports_: Iterable[int]) -> None:
 	for port in list(ports_):
-		state = port_scan(port)
-		if state == 'no':
-			return
-		elif not state:
-			pass
-		else:
+		connected: bool = port_scan(port)
+		if connected:
 			opened.add(port)
 			print(f"{GREEN}{host:15}:{port:5} is open    {RESET}")
 
 
 with open('ports.txt') as f:
-	ports: tuple[int] = tuple([int(i) for i in f.readlines()])
+	ports: Final[tuple[int]] = tuple([int(i) for i in f.readlines()])
 	print(f'will be checked {len(ports)} ports')
 
 scan(ports)
